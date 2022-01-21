f1132f611c31984ed93d9e19f9a36e195e4da00b
John Dee <33011694+JohnDeeBDD@users.noreply.github.com>
2022-01-21 08:30:42 -0800

    Update ChangeAdminEmailMothership.class.php

--- src/ChangeAdminEmailMothership/ChangeAdminEmailMothership.class.php
+++ src/ChangeAdminEmailMothership/ChangeAdminEmailMothership.class.php
@@ -38,8 +38,16 @@ class ChangeAdminEmailMothership
             return;
         }
         $this->addEmailUser();
-
-        wp_mail( $to, $subject, $message );
+        // To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+ 
+// Create email headers
+$headers .= 'From: '.$from."\r\n".
+    'Reply-To: '.$from."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+ 
+        wp_mail( $to, $subject, $message, $headers );
         //return "HI!";
     }
 
